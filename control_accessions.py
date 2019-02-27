fa142f8d392d8052fe40a732ac030769141de632
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-02-27 13:50:41 +0100

    Update control_accessions.py

--- control_accessions.py
+++ control_accessions.py
@@ -8,7 +8,7 @@ directories_list = glob.glob('*')
 ebi_accessions = metadata.ebi_accession.unique()
 
 with open('missing.csv', 'w') as out:
-    out.write('ebi_accession\tn_metadata\tn_sequences\tn_fna_files\tmissing\n')
+    out.write('ebi_accession\tn_metadata\tn_samples\tn_fna_files\tmissing\n')
     count = 0
     for accession in ebi_accessions:
 
@@ -16,14 +16,14 @@ with open('missing.csv', 'w') as out:
         n_metadata = len(accession_data)
 
         if accession in directories_list:
-            n_sequences = len(glob.glob(accession + '/*'))
+            n_samples = len(glob.glob(accession + '/*'))
             n_fna_files = len(glob.glob(accession + '/*/*.fna'))
             fna_files_list = glob.glob(accession + '/*/*.fna')
 
             if n_metadata == n_sequences and n_metadata == n_fna_files:
-                out.write('\t'.join([accession, str(n_metadata), str(n_sequences), str(n_fna_files), 'No', '\n']))
+                out.write('\t'.join([accession, str(n_metadata), str(n_samples), str(n_fna_files), 'No', '\n']))
             else:
-                out.write('\t'.join([accession, str(n_metadata), str(n_sequences), str(n_fna_files), 'Yes', '\n']))
+                out.write('\t'.join([accession, str(n_metadata), str(n_samples), str(n_fna_files), 'Yes', '\n']))
 
         else:
             out.write('\t'.join([accession, str(n_metadata), 'NA', 'NA', 'Yes', '\n']))
