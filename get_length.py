944e11d659ce0be78aceea4904afa6040a5bd110
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-03-01 10:03:46 +0100

    Update get_length.py

--- get_length.py
+++ get_length.py
@@ -4,6 +4,8 @@ from Bio import SeqIO
 import pandas as pd
 import matplotlib.pyplot as plt
 from multiprocessing import Pool
+import numpy as np
+import seaborn as sns
 
 n_cores = 24
 data_inputs = glob.glob('*')
@@ -39,3 +41,28 @@ pool = Pool(n_cores)
 pool.map(ProcessProject, data_inputs)
 pool.close()
 pool.join()
+
+with open('full_length.tsv', 'w') as out:
+    out.write('ProjectID\tSample_size\tMeanLength' + '\t'.join([i for i in range(1,300) + '\n']))
+    for project in data_inputs:
+        col_names = ['SampleID', 'SequenceID', 'Length']
+        data = pd.read_csv(file, sep='\t', header=1, names = col_names, na_values = 'NA', dtype={'SampleID': str, 'SequenceID': str, 'Length': int})
+
+        current_results = []
+        current_results.append(project)
+        current_results.append(len(data))
+        current_results.append(data['Length'].mean())
+        for i in range(1,300):
+            current_results.append(len(data[data['Length'] == i]))
+
+        out.write('\t'.join(current_results) + '\n')
+
+with open('full_length.tsv') as data:
+    col_names = ['ProjectID', 'Sample_size', 'MeanLength'] + [i for i in range(1,300)]
+    data = pd.read_csv(file, sep='\t', header=1, names = col_names, na_values = 'NA'})
+    values = data[[i for i in range(1,300)]]
+
+    plt.figure()
+    sns.heatmap(values)
+    plt.savefig('heatmap.png')
+    plt.close()
