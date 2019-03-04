f8fc24552b537cedcde0dd4e06553543231d6022
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-03-04 14:53:47 +0100

    Update create_manifest_SMA.py

--- create_manifest_SMA.py
+++ create_manifest_SMA.py
@@ -1,6 +1,17 @@
+import glob
+
 with open('SMA_manifest.csv', 'w') as out:
+
+    files_list = glob.glob('reads/*.fastq.gz')
+    id_set = set()
+    for file in files_list:
+        name = file.split('/')[-1].replace('_R1.fastq.gz','').replace('_R2.fastq.gz','')
+        id_set.add(name)
+
+    id_list = list(id_set)
+
     out.write('sample-id,absolute-filepath,direction\n')
 
-    for i in range(1,270):
-        out.write(','.join([str(i), str(i) + '_R1.fastq.gz', 'forward\n']))
-        out.write(','.join([str(i), str(i) + '_R2.fastq.gz', 'reverse\n']))
+    for i in id_list:
+        out.write(','.join([str(i), 'reads/' + str(i) + '_R1.fastq.gz', 'forward\n']))
+        out.write(','.join([str(i), 'reads/' + str(i) + '_R2.fastq.gz', 'reverse\n']))
