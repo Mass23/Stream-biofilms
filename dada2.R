cfdab2e46a34c1c1b5e5c2a9674318feb113b11e
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-03-06 09:35:52 +0100

    Create dada2.R

---
+++ dada2.R
@@ -0,0 +1,26 @@
+library(dada2)
+
+reads_path <- ('/home/fodelian/Desktop/SMA/reads')
+
+fnFs <- sort(list.files(path, pattern="_forward_paired.fq.gz", full.names = TRUE))
+fnRs <- sort(list.files(path, pattern="_reverse_paired.fq.gz", full.names = TRUE))
+
+sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
+
+# Place filtered files in filtered/ subdirectory
+filtFs <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
+filtRs <- file.path(path, "filtered", paste0(sample.names, "_R_filt.fastq.gz"))
+out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, compress=TRUE, multithread=TRUE) # On Windows set multithread=FALSE
+
+derepFs <- derepFastq(filtFs, verbose=TRUE)
+derepRs <- derepFastq(filtRs, verbose=TRUE)
+# Name the derep-class objects by the sample names
+names(derepFs) <- sample.names
+names(derepRs) <- sample.names
+
+dadaFs <- dada(derepFs, err=errF, multithread=TRUE)
+dadaRs <- dada(derepRs, err=errR, multithread=TRUE)
+
+mergers <- mergePairs(dadaFs, derepFs, dadaRs, derepRs, verbose=TRUE, maxMismatch=1)
+
+write.fasta(mergers, file = "merged_reads.fasta")
