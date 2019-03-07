1411b904fdb8cd192d35497aec59df9303a4128b
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-03-07 12:37:14 +0100

    Create q2_dada2.sh

---
+++ q2_dada2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source activate qiime2-2019.1
+
+qiime dada2 denoise-paired \
+  --i-demultiplexed-seqs DATASET_raw.qza \
+  --p-n-threads 16 \
+  --p-trunc-len-f 0 \
+  --o-table DATASET_dada2_table.qza \
+  --o-representative-sequences DATASET_dada2_seqs.qza \
+  --o-denoising-stats DATASET_dada2_stats.qza \
+
+qiime metadata tabulate \
+  --m-input-file DATASET_dada2_stats.qza \
+  --o-visualization DATASET_dada2_stats.qzv
+
+qiime feature-table summarize \
+  --i-table DATASET_dada2_table.qza \
+  --o-visualization DATASET_dada2_table.qzv \
+  --m-sample-metadata-file sample-metadata.tsv
+
+qiime feature-table tabulate-seqs \
+  --i-data DATASET_dada2_seqs.qza \
+  --o-visualization DATASET_dada2_seqs.qzv
