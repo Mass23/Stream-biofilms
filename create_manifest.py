976c006a7003d2ce3ac61b2e1a4c0d3ebe9d3776
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-03-08 09:30:56 +0100

    Update create_manifest.py

--- create_manifest.py
+++ create_manifest.py
@@ -1,13 +1,22 @@
 import glob
 import os
+import argparse
+
+# python3 create_manifest.py -i NAME
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-i', '--id', help='ID of the dataset', type=str, action = 'store', required = True)
+args = parser.parse_args()
+n_cores = str(args.NumberCores)
+
 cwd = os.getcwd()
 
 with open('SMA_manifest.csv', 'w') as out:
 
-    files_list = glob.glob('reads/*.fq.gz')
+    files_list = glob.glob('reads/*.fastq.gz')
     id_set = set()
     for file in files_list:
-        name = file.split('/')[-1].replace('_forward_paired.fq.gz','').replace('_reverse_paired.fq.gz','')
+        name = file.split('/')[-1].replace('_R1.fastq.gz','').replace('_R2.fastq.gz','')
         id_set.add(name)
 
     id_list = list(id_set)
@@ -15,5 +24,5 @@ with open('SMA_manifest.csv', 'w') as out:
     out.write('sample-id,absolute-filepath,direction\n')
 
     for i in id_list:
-        out.write(','.join([str(i), cwd + '/reads/' + str(i) + '_forward_paired.fq.gz', 'forward\n']))
-out.write(','.join([str(i), cwd + '/reads/' + str(i) + '_reverse_paired.fq.gz', 'reverse\n']))
+        out.write(','.join([str(i), cwd + '/reads/' + str(i) + '_R1.fastq.gz', 'forward\n']))
+        out.write(','.join([str(i), cwd + '/reads/' + str(i) + '_R2.fastq.gz', 'reverse\n']))
