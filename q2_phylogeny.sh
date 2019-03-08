52c0baad9386b2a3be5fd0caeeabc3c599335231
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-03-08 10:41:10 +0100

    Update q2_phylogeny.sh

--- q2_phylogeny.sh
+++ q2_phylogeny.sh
@@ -1,24 +1,34 @@
-#!/bin/bash
+import subprocess
+import argparse
 
-source activate qiime2-2019.1
+# python3 q2_phylogeny.py -i DATASET_NAME -n N_THREADS -b N_BOOTSTRAP
+parser = argparse.ArgumentParser()
 
-qiime alignment mafft \
-  --i-sequences DATASET_dada2_seqs.qza \
-  --p-n-threads 16 \
-  --o-alignment phylogeny/DATASET_aln_seqs.qza \
+parser.add_argument('-i', '--id', help='ID of the dataset', type=str, action = 'store', required = True)
+parser.add_argument('-n', '--Nthreads', help='Number of threads to use', type=int, action = 'store', required = True)
 
-qiime alignment mask \
-  --i-alignment phylogeny/DATASET_aln_seqs.qza \
-  --o-masked-alignment phylogeny/DATASET_masked_aln_seqs.qza
+parser.add_argument('-b', '--BootStraps', help='Number of bootstraps for the RAxML inference.', type=int, action = 'store', required = True)
 
-qiime phylogeny raxml-rapid-bootstrap \
-  --i-alignment phylogeny/DATASET_masked_aln_seqs.qza \
-  --p-bootstrap-replicates 100 \
-  --p-n-threads 16 \
-  --p-raxml-version AVX2 \
-  --p-substitution-model GTRCAT \
-  --o-tree phylogeny/DATASET_GTRCAT_100bs.qza
+args = parser.parse_args()
 
-qiime phylogeny midpoint-root \
-  --i-tree phylogeny/DATASET_GTRCAT_100bs.qza \
-  --o-rooted-tree phylogeny/DATASET_GTRCAT_100bs_rooted.qza
+dataset_id = args.id
+n_threads = args.Nthreads
+n_bootstraps = args.BootStraps
+
+mafft_args = ['qiime', 'alignment', 'mafft', '--i-sequences', dataset_id + '_dada2_seqs.qza', '--p-n-threads', n_threads, '--o-alignment', 'phylogeny/' + dataset_id + '_aln_seqs.qza']
+subprocess.call(' '.join(mafft_args), shell = True)
+
+mask_args = ['qiime', 'alignment', 'mask', '--i-alignment', 'phylogeny/' + dataset_id + '_aln_seqs.qza', '--o-masked-alignment', 'phylogeny/' + dataset_id + '_masked_aln_seqs.qza']
+subprocess.call(' '.join(mask_args), shell = True)
+
+raxml_args = ['qiime', 'phylogeny', 'raxml-rapid-bootstrap',
+    '--i-alignment', 'phylogeny/' + dataset_id + '_masked_aln_seqs.qza',
+    '--p-bootstrap-replicates', n_bootstraps,
+    '--p-n-threads', n_threads,
+    '--p-raxml-version', 'AVX2',
+    '--p-substitution-model', 'GTRCAT',
+    '--o-tree', 'phylogeny/' + dataset_id + '_GTRCAT_100bs.qza']
+subprocess.call(' '.join(raxml_args), shell = True)
+
+root_args = ['qiime', 'phylogeny', 'midpoint-root', '--i-tree', 'phylogeny/' + dataset_id + '_GTRCAT_100bs.qza', '--o-rooted-tree', 'phylogeny/' + dataset_id + '_GTRCAT_100bs_rooted.qza']
+subprocess.call(' '.join(root_args), shell = True)
