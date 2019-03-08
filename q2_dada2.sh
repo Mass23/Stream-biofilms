8ce28cab4dd72f321441dd1d32c4b55ac68412bd
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-03-08 10:21:46 +0100

    Update q2_dada2.sh

--- q2_dada2.sh
+++ q2_dada2.sh
@@ -1,22 +1,42 @@
-#!/bin/bash
+import subprocess
+import argparse
 
-source activate qiime2-2019.1
+# python3 q2_denoise.py -i DATASET_NAME -n Ncores -lf 20 -rf 20 -lr 20 -rr 20
+parser = argparse.ArgumentParser()
 
-qiime dada2 denoise-paired \
-  --i-demultiplexed-seqs DATASET_raw.qza \
-  --p-n-threads 16 \
-  --p-trim-left-f int \
-  --p-trunc-len-f int \
-  --p-trim-left-r int \
-  --p-trunc-len-r int \
-  --o-table DATASET_dada2_table.qza \
-  --o-representative-sequences DATASET_dada2_seqs.qza \
-  --o-denoising-stats DATASET_dada2_stats.qza \
+parser.add_argument('-i', '--id', help='ID of the dataset', type=str, action = 'store', required = True)
+parser.add_argument('-n', '--Nthreads', help='Number of threads to use', type=int, action = 'store', required = True)
 
-qiime metadata tabulate \
-  --m-input-file DATASET_dada2_stats.qza \
-  --o-visualization DATASET_dada2_stats.qza.qzv
+parser.add_argument('-lf', '--TrimLeftForward', help='Index to trim the left part of forward reads.', type=int, action = 'store', required = True)
+parser.add_argument('-rf', '--TrimRightForward', help='Index to trim the right part of forward reads.', type=int, action = 'store', required = True)
+parser.add_argument('-lr', '--TrimLeftReverse', help='Index to trim the left part of reverse reads.', type=int, action = 'store', required = True)
+parser.add_argument('-rr', '--TrimRightReverse', help='Index to trim the right part of reverse reads.', type=int, action = 'store', required = True)
 
-qiime feature-table tabulate-seqs \
-  --i-data DATASET_dada2_seqs.qza \
-  --o-visualization DATASET_dada2_seqs.qzv
+args = parser.parse_args()
+
+dataset_id = args.id
+n_threads = args.Nthreads
+
+lf = args.TrimLeftForward
+rf = args.TrimRightForward
+lr = args.TrimLeftReverse
+rr = args.TrimRightReverse
+
+denoise_args = ['qiime','dada2', 'denoise-paired',
+  '--i-demultiplexed-seqs', dataset_id + '_raw.qza',
+  '--p-n-threads', str(n_threads),
+  '--p-trim-left-f', str(lf),
+  '--p-trunc-len-f', str(rf),
+  '--p-trim-left-r', str(lr),
+  '--p-trunc-len-r', str(rr),
+  '--o-table', dataset_id + '_dada2_table.qza',
+  '--o-representative-sequences', dataset_id + '_dada2_seqs.qza',
+  '--o-denoising-stats', dataset_id + '_dada2_stats.qza']
+subprocess.call(' '.join(denoise_args), shell = True)
+
+stats_args = ['qiime', 'metadata', 'tabulate', '--m-input-file', dataset_id + '_dada2_stats.qza', '--o-visualization', dataset_id + '_dada2_stats.qzv']
+subprocess.call(' '.join(stats_args), shell = True)
+
+seqs_args = ['qiime', 'feature-table', 'tabulate-seqs', '--i-data', dataset_id + '_dada2_seqs.qza', '--o-visualization', dataset_id + '_dada2_seqs.qzv']
+subprocess.call(' '.join(seqs_args), shell = True)
+
